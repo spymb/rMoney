d4ad93395f959f1baeb988046c4446af2b63014d
spymb <spym1933@foxmail.com>
2022-02-14 20:00:21 +0800

    页面切换优化

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,25 +1,38 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
 import React from 'react';
-
 import Icon from './Icon';
+import {NavLink} from 'react-router-dom';
 
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
-    display:flex;
-    > li{
+    display: flex;
+
+    > li {
       width: 33.3333%;
-      text-align:center;
-      display: flex;
-      flex-direction: column;
-      padding: 4px 0;
-      justify-content: center;
-      align-items: center;
-      .icon {
-        width: 24px;
-        height: 24px;
+      text-align: center;
+
+      > a {
+        display: flex;
+        flex-direction: column;
+        padding: 4px 0;
+        justify-content: center;
+        align-items: center;
+
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
+
+        &.active {
+          color: red;
+
+          .icon {
+            fill: red;
+          }
+        }
       }
     }
   }
@@ -30,16 +43,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag"/>
-          <Link to="/tags">标签页</Link>
+          <NavLink to="/tags">
+            <Icon name="tag"/>
+            标签页
+          </NavLink>
         </li>
         <li>
-          <Icon name="money"/>
-          <Link to="/money">记账页</Link>
+          <NavLink to="/money">
+            <Icon name="money"/>
+            记账页
+          </NavLink>
         </li>
         <li>
-          <Icon name="chart"/>
-          <Link to="/statistics">统计页</Link>
+          <NavLink to="/statistics">
+            <Icon name="chart"/>
+            统计页
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
