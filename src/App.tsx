2498c73e12201cf0b7bbb75e9add20d90542567b
spymb <spym1933@foxmail.com>
2022-02-16 00:10:25 +0800

    修复id不断自增的bug

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/tags" element={<Tags/>}/>
-          <Route path="/tags/:tag" element={<Tag/>}/>
+          <Route path="/tags/:id" element={<Tag/>}/>
           <Route path="/money" element={<Money/>}/>
           <Route path="/statistics" element={<Statistics/>}/>
           <Route path="/" element={<Navigate replace to="/money"/>}/>
--- src/views/tags/useTags.tsx
+++ src/views/tags/useTags.tsx
@@ -1,13 +1,15 @@
 import {useState} from 'react';
 import {createID} from '../../lib/createID';
 
+const defaultTags = [
+  {id: createID(), name: '衣'},
+  {id: createID(), name: '食'},
+  {id: createID(), name: '住'},
+  {id: createID(), name: '行'},
+];
+
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number; name: string }[]>([
-    {id: createID(), name: '衣'},
-    {id: createID(), name: '食'},
-    {id: createID(), name: '住'},
-    {id: createID(), name: '行'},
-  ]);
+  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
 
   return {tags, setTags};
 };
