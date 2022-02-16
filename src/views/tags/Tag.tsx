8701744ff648789817218de29dc1810d30d0ab23
spymb <spym1933@foxmail.com>
2022-02-16 13:11:43 +0800

    Tag页面布局

--- src/views/tags/Tag.tsx
+++ src/views/tags/Tag.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import {useTags} from './useTags';
 import {useParams} from 'react-router-dom';
+import styled from 'styled-components';
+import Layout from '../../components/Layout';
+import Icon from '../../components/Icon';
+import {Input} from '../../components/Input';
+import {Center} from '../../components/Center';
+import {Space} from '../../components/Space';
+import { Button } from 'components/Button';
+
+const Topbar = styled.header`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background:white;
+`;
+
+const InputWrapper = styled.div`
+  background:white;
+  padding: 0 16px;
+  margin-top: 8px;
+`;
 
 const Tag: React.FC = () => {
   const {findTag} = useTags();
@@ -8,8 +30,25 @@ const Tag: React.FC = () => {
   const tag = findTag(parseInt(id as string));
 
   return (
-    <div>{tag.name}</div>
-  );
+    <Layout>
+      <Topbar>
+        <Icon name="left"/>
+        <span>编辑标签</span>
+        <Icon/>
+      </Topbar>
+
+      <InputWrapper>
+        <Input label="标签名" type="text" placeholder="标签名" value={tag.name}/>
+      </InputWrapper>
+
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>删除标签</Button>
+      </Center>
+    </Layout>
+  )
 };
 
 export {Tag};
