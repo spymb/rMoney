2f475c33d8ca2cae3d21dad65b291050f653a275
spymb <spym1933@foxmail.com>
2022-02-16 20:31:19 +0800

    引入classnames

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cs from 'classnames'
 
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
@@ -8,9 +9,9 @@ type Props = {
 } & React.SVGAttributes<SVGElement>
 
 const Icon = (props: Props) => {
-  const {name, children, ...rest} = props
+  const {name, className, children, ...rest} = props
   return (
-    <svg className="icon" {...rest}>
+    <svg className={cs('icon', className)} {...rest}>
       {props.name && <use xlinkHref={'#' + props.name}/>}
     </svg>
   );
